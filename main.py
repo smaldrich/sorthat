03b2aaefac362a06a4a76fc5bd995964ad8e28de
robbie aldrich <robbiemaldrich@gmail.com>
2025-02-11 16:57:22 -0600

    much better

--- main.py
+++ main.py
@@ -6,28 +6,9 @@ import sys
 assert len(sys.argv) == 2
 
 
-class Link:
-    def __init__(self, p1: str, p2: str):
-        self.p1 = min(p1, p2)
-        self.p2 = max(p1, p2)
-        self.strength = 1
-
-    def __eq__(self, value):
-        return self.p1 == value.p1 and self.p2 == value.p2
-
-    def __hash__(self):
-        return self.p1.__hash__() + self.p2.__hash__()
-
-    def __str__(self):
-        return f"{self.p1} <-> {self.p2}"
-        pass
-
-
-og_links: dict[Link, int] = {}
-og_people: list[str] = []
-og_wants: dict[str, list[str]] = {}
+og_people_list: list[str] = []
 og_genders: dict[str, str] = {}
-og_people_by_least_wants: list[tuple[str, int]] = []
+og_wants: dict[str, list[str]] = {}
 with open(sys.argv[1], newline="") as f:
     reader = csv.reader(f)
     for i, line in enumerate(reader):
@@ -38,110 +19,128 @@ with open(sys.argv[1], newline="") as f:
         wants: list[str] = line[2].lower().split(", ")
         wants = [x.strip() for x in wants]
 
-        og_people.append(name)
+        og_people_list.append(name)
         og_genders[name] = gender
         og_wants[name] = wants
-        for r in wants:
-            link = Link(name, r)
-            if link in og_links:
-                og_links[link] += 1
-            else:
-                og_links[link] = 1
 
-want_counts: dict[str, int] = {}
-for person in og_people:
-    count: int = 0
-    for other in og_people:
+for person in og_people_list:
+    gender = og_genders[person]
+    bad: list[str] = []
+    for other in og_wants[person]:
+        if other not in og_people_list:
+            print(f"invalid want from {person} to {other}")
+            bad.append(other)
+            continue
+        elif og_genders[other] != gender:
+            print(f"invalid want from {person} to {other}")
+            bad.append(other)
+            continue
+
+    for x in bad:
+        og_wants[person].remove(x)
+
+og_wants_counts: dict[str, int] = {}
+for person in og_people_list:
+    og_wants_counts[person] = 0
+    for other in og_people_list:
         if person in og_wants[other]:
-            count += 1
-    want_counts[person] = count
-
-sorted_people = og_people.copy()
-sorted_people.sort(key=lambda x: want_counts[x])
+            og_wants_counts[person] += 1
 
 # END FILE IMPORT
 
-# CULLING BAD LINKS
+og_strong_pairs: list[tuple[str, str]] = []
+for person in og_people_list:
+    for other in og_people_list:
+        if other not in og_wants[person]:
+            continue
+        elif person not in og_wants[other]:
+            continue
+        pair = (min(other, person), max(other, person))
+        if pair not in og_strong_pairs:
+            og_strong_pairs.append(pair)
+og_strong_pairs.sort(key=lambda x: og_wants_counts[x[0]] + og_wants_counts[x[1]])
 
-bad_links: list[Link] = []
-for link in og_links:
-    if link.p1 not in og_people or link.p2 not in og_people:
-        print(f"link with an invalid name: {link}, removing.")
-        bad_links.append(link)
-    elif og_genders[link.p1] != og_genders[link.p2]:
-        print(f"invalid gender link: {link}, removing.")
-        bad_links.append(link)
-    elif og_genders[link.p1] != og_genders[link.p2]:
-        bad_links.append(link)
+og_adjacents: dict[str, set[str]] = {}
+for person in og_people_list:
+    og_adjacents[person] = set()
+for person in og_people_list:
+    for other in og_wants[person]:
+        og_adjacents[person].add(other)
+        og_adjacents[other].add(person)
 
-for link in bad_links:
-    # FIXME: file src
-    og_links.pop(link)
 
-# END CULLING BAD LINKS
+def gen_rooms():
+    all_rooms: list[list[str]] = []
+    people_remaining = og_people_list.copy()
+    while len(people_remaining) > 0:
+        people_in_room: list[str] = []
 
-all_rooms: list[list[str]] = []
-while len(og_links) > 0:
-    links_in_room = []
-    people_in_room = set()
-    for link, strength in og_links.items():
-        if strength == 2:
-            links_in_room.append(link)
-            people_in_room.add(link.p1)
-            people_in_room.add(link.p2)
-            break
-
-    if len(links_in_room) == 0:
-        links_in_room.append(og_links.popitem()[0])
-        people_in_room.add(link.p1)
-        people_in_room.add(link.p2)
-
-    out_of_links = False
-    while len(people_in_room) < 4 and not out_of_links:
-        for floater in sorted_people:
-            if floater in people_in_room:
-                continue
-
-            found = None
-            for link in og_links:
-                if link in links_in_room:
-                    continue
-                elif link.p1 == floater or link.p2 == floater:
-                    if link.p1 in people_in_room or link.p2 in people_in_room:
-                        found = link
-                        break
-            if found is None:
-                out_of_links = True
+        for pair in og_strong_pairs:
+            if pair[0] in people_remaining and pair[1] in people_remaining:
+                people_in_room.append(pair[0])
+                people_in_room.append(pair[1])
+                print(f"selecting base pair {pair[0]} <-> {pair[1]}")
                 break
 
-            people_in_room.add(found.p1)
-            people_in_room.add(found.p2)
-            links_in_room.append(found)
-            break
+        if len(people_in_room) == 0:
+            print(f"base pair failed. selecting {people_remaining[0]}")
+            people_in_room = [people_remaining[0]]
 
-    # remove any links containing ppl in this room before moving on
-    bad_links = set()
-    for person in people_in_room:
-        sorted_people.remove(person)
-        for link in og_links:
-            if person == link.p1 or person == link.p2:
-                bad_links.add(link)
-    for link in bad_links:
-        og_links.pop(link)
+        while len(people_in_room) < 4:
+            adjacents: set[str] = set()
+            for person in people_in_room:
+                adjacents = adjacents.union(og_adjacents[person])
 
+            bad: list[str] = []
+            for adj in adjacents:
+                if adj in people_in_room:
+                    bad.append(adj)
+                elif adj not in people_remaining:
+                    bad.append(adj)
+            for x in bad:
+                adjacents.remove(x)
+
+            if len(adjacents) == 0:
+                break  # break finding ppl for the room
+
+            min_adjacent: str | None = None
+            min_want_count = 100000000000
+            for adj in adjacents:
+                count = og_wants_counts[adj]
+                # count = min(count, len(og_wants[adj]))
+                if count < min_want_count:
+                    min_adjacent = adj
+                    min_want_count = count
+
+            assert min_adjacent is not None
+            print(f"adding {min_adjacent} with outer count {min_want_count}.")
+            print(f"from pool: {adjacents}")
+            people_in_room.append(min_adjacent)
+        # end searching for the room
+
+        all_rooms.append(people_in_room)
+        for person in people_in_room:
+            people_remaining.remove(person)
+
+        print(f"Adding room: {people_in_room}\n")
+
+    return all_rooms
+
+
+print("\nRooms:")
+all_rooms = gen_rooms()
 for room in all_rooms:
     print(room)
 
-# PRINTING ISSUES
-print("\nIssues:")
-
-for p in og_people:
+problem_count: int = 0
+for p in og_people_list:
     person_room = None
     for room in all_rooms:
         if p in room:
             person_room = room
 
     if person_room is None:
+        problem_count += 1
         print(f"{p} didn't get a room!")
         continue
 
@@ -151,19 +150,22 @@ for p in og_people:
             score += 1
 
     if score < 1:
+        problem_count += 1
         print(f"{p} got {score} matches.")
 
 for i, r in enumerate(all_rooms):
     if len(r) < 3:
+        problem_count += 1
         print(f"room {i} only has {len(r)} people.")
-# END PRINTING ISSUES
+print(f"{problem_count} problem(s)")
 
-f = open("rooms.csv", "w")
-for room in all_rooms:
-    line = ""
-    for person in room:
-        line = line + person + ","
-    line = line + "\n"
-    f.write(line)
 
-print("outputted rooms written to 'rooms.csv'")
+# f = open("rooms.csv", "w")
+# for room in all_rooms:
+#     line = ""
+#     for person in room:
+#         line = line + person + ","
+#     line = line + "\n"
+#     f.write(line)
+
+# print("outputted rooms written to 'rooms.csv'")
