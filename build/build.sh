bd48ae9cb2ed2dcda61bc156bf9a9edf7cc7ae02
robbie aldrich <robbiemaldrich@gmail.com>
2025-02-18 21:48:49 -0600

    commented NFD from the build

--- build/build.sh
+++ build/build.sh
@@ -6,9 +6,9 @@
 # gcc -c external/stb/stb_impl.c -Iexternal -o out/stb.o -g -Wall
 # echo "STB built"
 
-gcc -c external/nfd/nfd_common.c -Iexternal/nfd -Iexternal/nfd/include -o out/nfd_common.o -g -Wall
-gcc -c external/nfd/nfd_win.cpp -Iexternal/nfd -Iexternal/nfd/include -o out/nfd_win.o -g -Wall
-echo "nfd built"
+# gcc -c external/nfd/nfd_common.c -Iexternal/nfd -Iexternal/nfd/include -o out/nfd_common.o -g -Wall
+# gcc -c external/nfd/nfd_win.cpp -Iexternal/nfd -Iexternal/nfd/include -o out/nfd_win.o -g -Wall
+# echo "nfd built"
 
 gcc -c src/main.c -o out/main.o -g -Wall -pedantic -Wextra -Werror -Iexternal -Isrc
 echo "main built"
